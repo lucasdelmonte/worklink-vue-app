fee1c994c928e4b0a89efeee91ed902712d500c4
Lucas Delmonte <82593976+lucasdelmonte@users.noreply.github.com>
2024-01-16 19:42:17 -0300

    vue session deleted, vue cookies installed

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import VueCookies from 'vue3-cookies'
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+createApp(App).use(router).use(createPinia()).use(VueCookies).mount('#app')
