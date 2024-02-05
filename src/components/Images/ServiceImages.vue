3572afd781dcf564830a36df9417546df62f1746
Lucas Delmonte <82593976+lucasdelmonte@users.noreply.github.com>
2024-02-04 23:34:28 -0300

    presentational pattern in service images gallery

---
+++ src/components/Images/ServiceImages.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="gallery__image">
+    <img v-for="(url, index) in images" :src="url" :key="index" alt="Image" />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { defineProps } from "vue"
+
+  const { images } = defineProps(["images"])
+</script>
+
+<style lang="scss">
+  .gallery {
+    &__image {
+    flex: 0 0 40%;
+    width: 40%;
+    object-fit: cover;
+    scroll-snap-align: center;
+    display: flex;
+
+    img {
+      width: 100%;
+      object-fit: contain;
+    }
+  }
+  }
+</style>
